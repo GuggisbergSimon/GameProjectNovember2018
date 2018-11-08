96b191a586a226db919277694ab873b7146ffb2a
Unknown <slyguggi@gmail.com>
2018-11-08 15:04:02 +0100

    Fixed player speed, to test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,25 +8,23 @@ public class Player : MonoBehaviour
 	private Rigidbody2D myRigidbody2D;
 	private bool isInvincible = false;
 	private SpriteRenderer image;
-	private float capSpeed;
+	private float speed;
 	private int cargo;
 
 	[SerializeField] private int maxCargo = 1000;
 	[SerializeField] private Image cargoGauge;
-	[SerializeField] private float sideSpeed = 0.1f;
-	[SerializeField] private float upSpeed = 0.1f;
-	[SerializeField] private float maxSpeed = 20;
+	[SerializeField] private float maxSpeed = 10;
+	[SerializeField] private float slowSpeed = 5;
 	[SerializeField] private float maxInvincibilityTime = 2;
 	[SerializeField] private float InvincibilityBlinkInterval = 0.2f;
 	[SerializeField] private GameManager gameManager;
-	[SerializeField] private float slowSpeed = 10;
 
 	private void Start()
 	{
 		myRigidbody2D = GetComponent<Rigidbody2D>();
 		image = GetComponentInChildren<SpriteRenderer>();
 		cargo = maxCargo;
-		capSpeed = maxSpeed;
+		speed = maxSpeed;
 		if (InvincibilityBlinkInterval > maxInvincibilityTime)
 		{
 			InvincibilityBlinkInterval = maxInvincibilityTime;
@@ -37,32 +34,23 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	private void Update()
 	{
-		Vector2 v = Vector2.up * Input.GetAxis("Vertical") * upSpeed * Time.deltaTime;
-		Vector2 h = Vector2.right * Input.GetAxis("Horizontal") * sideSpeed * Time.deltaTime;
-		Vector2 myVelocity = v + h;
+		Vector2 v = Vector2.up * Input.GetAxis("Vertical");
+		Vector2 h = Vector2.right * Input.GetAxis("Horizontal");
 
+		//TODO reimplement that
 		//set capspeed to slow or max speed wether the related button has been pressed or released
 		if (Input.GetButton("Fire1"))
 		{
-			capSpeed = slowSpeed;
+			speed = slowSpeed;
 		}
 		else if (Input.GetButtonUp("Fire1"))
 		{
-			capSpeed = maxSpeed;
+			speed = maxSpeed;
 		}
 
-		//checks for capspeed
-		if (Mathf.Abs(myVelocity.x) > capSpeed)
-		{
-			myVelocity.x = Mathf.Sign(myVelocity.x) * capSpeed;
-		}
-
-		if (Mathf.Abs(myVelocity.y) > capSpeed)
-		{
-			myVelocity.y = Mathf.Sign(myVelocity.y) * capSpeed;
-		}
-
-		myRigidbody2D.velocity = myVelocity;
+		Vector2 nextPos = (v + h) * speed * Time.deltaTime;
+		Debug.Log(nextPos);
+		myRigidbody2D.MovePosition(transform.position + (Vector3) nextPos);
 	}
 
 	private void OnTriggerStay2D(Collider2D other)
