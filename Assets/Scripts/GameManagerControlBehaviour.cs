0a1041fc0cb7abe64863ca23c828ada78303fd72
Unknown <slyguggi@gmail.com>
2018-11-26 12:26:22 +0100

    Added timeline for patterns

---
+++ Assets/Scripts/GameManagerControlBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GameManagerControlBehaviour : PlayableBehaviour
+{
+	private GameManager gameManager;
+	private GameObject[] patternToShoot;
+
+	public GameManager GameManager
+	{
+		get { return gameManager; }
+		set { gameManager = value; }
+	}
+
+	public GameObject[] PatternToShoot
+	{
+		get { return patternToShoot; }
+		set { patternToShoot = value; }
+	}
+
+	public override void OnBehaviourPlay(Playable playable, FrameData info)
+	{
+		for (int i = 0; i < PatternToShoot.Length; i++)
+		{
+			gameManager.Fire(PatternToShoot[i]);
+		}
+	}
+}
